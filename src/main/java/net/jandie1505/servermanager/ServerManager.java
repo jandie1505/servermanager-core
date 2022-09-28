0bcd0be90bb674a9f22c0c56818e21244e7dc968
jandie1505 <jandie1505@jandie1505.net>
2022-09-28 13:55:34 +0200

    initial commit

---
+++ src/main/java/net/jandie1505/servermanager/ServerManager.java
@@ -0,0 +1,14 @@
+package net.jandie1505.servermanager;
+
+public class ServerManager {
+
+    public ServerManager() {
+
+    }
+
+    public static ServerManager instance;
+
+    public static void main(String[] args) {
+        instance = new ServerManager();
+    }
+}
