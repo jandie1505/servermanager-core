15b22722b8580e086d38e7bf03d57b0d2a658313
jandie1505 <jandie1505@jandie1505.net>
2022-10-01 17:40:44 +0200

    plugin services

--- src/main/java/net/jandie1505/servermanager/plugins/PluginHandler.java
+++ src/main/java/net/jandie1505/servermanager/plugins/PluginHandler.java
@@ -49,6 +49,41 @@ public final class PluginHandler {
     }
 
     /**
+     * Create a new repeating service.
+     * Services are running their code in a specified time interval if the plugin and the service is enabled.
+     * Don't use your own repeating threads, use these services feature instead.
+     * @param runnable PluginServiceRunnable (The same as a java runnable, but with the PluginService object as parameter
+     * @param time Time interval the service should be executed. Can be changed during runtime.
+     * @return PluginService
+     */
+    public PluginService createService(PluginServiceRunnable runnable, int time) {
+        PluginService pluginService = new PluginService(this, runnable, time);
+        this.services.add(pluginService);
+        return pluginService;
+    }
+
+    /**
+     * Remove a repeating service by id.
+     * The id is the list index of the service.
+     * If a service is running while it's being deleted, it will stop.
+     * @param serviceId list index of the service
+     */
+    public void deleteService(int serviceId) {
+        if (serviceId < this.services.size()) {
+            this.services.remove(serviceId);
+        }
+    }
+
+    /**
+     * Remove a repeating service by PluginService object.
+     * If a service is running while it's being deleted, it will stop.
+     * @param service The PluginService object of the service
+     */
+    public void deleteService(PluginService service) {
+        this.services.remove(service);
+    }
+
+    /**
      * Get the plugin manager
      * @return PluginManager
      */
@@ -72,6 +107,10 @@ public final class PluginHandler {
         return this.plugin;
     }
 
+    /**
+     * Get all services of this plugin
+     * @return List of services
+     */
     public List<Runnable> getServices() {
         return List.copyOf(this.services);
     }
