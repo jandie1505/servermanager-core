81c989e6713de0f84d36dd5e3c5157574eca9507
jandie1505 <jandie1505@jandie1505.net>
2022-10-02 15:44:56 +0200

    started new JDAManager

--- src/main/java/net/jandie1505/servermanager/bot/JDAManager.java
+++ src/main/java/net/jandie1505/servermanager/bot/JDAManager.java
@@ -1,47 +1,59 @@
 package net.jandie1505.servermanager.bot;
 
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
+import net.dv8tion.jda.api.utils.cache.ShardCacheView;
 import net.jandie1505.servermanager.ServerManager;
+import org.jetbrains.annotations.NotNull;
 
-public final class JDAManager {
+public final class JDAManager implements ShardManager {
     private final ServerManager serverManager;
-    private ShardManager shardManager;
 
     public JDAManager(ServerManager serverManager) {
         this.serverManager = serverManager;
     }
 
-    public void startShardManager() {
-        if (this.shardManager == null) {
-            this.shardManager = DefaultShardManagerBuilder
-                    .create(this.serverManager.getConfigManager().getConfig().getString("token"), GatewayIntent.getIntents(GatewayIntent.ALL_INTENTS))
-                    .setShardsTotal(1)
-                    .addEventListeners(new SystemEvents(this))
-                    .addEventListeners(new RedirectEvents(this))
-                    .setActivity(Activity.playing("Starting up..."))
-                    .setStatus(OnlineStatus.IDLE)
-                    .build();
-            this.serverManager.getLogger().info("Created shard manager");
-        }
-    }
-
-    public void stopShardManager() {
-        if (this.shardManager != null) {
-            this.shardManager.shutdown();
-            this.shardManager = null;
-            this.serverManager.getLogger().info("Shut down shard manager");
-        }
-    }
-
-    public ShardManager getShardManager() {
-        return this.shardManager;
-    }
-
     public ServerManager getServerManager() {
         return this.serverManager;
+    }
+
+    @Override
+    public int getShardsQueued() {
+        return 0;
+    }
+
+    @NotNull
+    @Override
+    public ShardCacheView getShardCache() {
+        return null;
+    }
+
+    @Override
+    public void restart() {
+
+    }
+
+    @Override
+    public void restart(int i) {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+
+    @Override
+    public void shutdown(int i) {
+
+    }
+
+    @Override
+    public void start(int i) {
+
+    }
+
+    @Override
+    public void login() {
+
     }
 }
